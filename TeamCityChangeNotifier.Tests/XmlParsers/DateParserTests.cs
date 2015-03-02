2fbdfc425e5566d8a9f3165bb24332ce53dba560
Anthony Steele <anthony.steele@7digital.com>
2015-03-02 09:32:20 +0000

    Aded test on date parse

---
+++ TeamCityChangeNotifier.Tests/XmlParsers/DateParserTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using TeamCityChangeNotifier.XmlParsers;
+
+namespace TeamCityChangeNotifier.Tests.XmlParsers
+{
+	[TestFixture]
+	public class DateParserTests
+	{
+		[Test]
+		public void TestParse()
+		{
+			var date = DateParser.Parse("20150218T114435+0000");
+
+			Assert.That(date.Year, Is.EqualTo(2015));
+			Assert.That(date.Month, Is.EqualTo(2));
+			Assert.That(date.Day, Is.EqualTo(18));
+			Assert.That(date.Hour, Is.EqualTo(11));
+			Assert.That(date.Minute, Is.EqualTo(44));
+			Assert.That(date.Second, Is.EqualTo(35));
+		}
+	}
+}
