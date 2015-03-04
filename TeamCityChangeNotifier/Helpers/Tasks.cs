510430faf973e835268196e6b532fe3e74494111
Anthony Steele <anthony.steele@7digital.com>
2015-03-04 08:57:02 +0000

    Comments in Tasks.cs

--- TeamCityChangeNotifier/Helpers/Tasks.cs
+++ TeamCityChangeNotifier/Helpers/Tasks.cs
@@ -7,6 +7,19 @@ namespace TeamCityChangeNotifier.Helpers
 {
 	public static class Tasks
 	{
+		/// <summary>
+		/// Not sure if this is great code or terrible code. But it works I think.
+		/// It aims to extract out a acommon pattern that I use regularly
+		/// i.e. given a list of inputs, and an async way to get outputs,
+		/// Get the outputs in parallel
+		/// e.g. In parallel, turn a list of Urls, (T is string) to responses 
+		/// (U is HttpResponseMessage and the read func wraps a HTTP get)
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <typeparam name="U"></typeparam>
+		/// <param name="inputs"></param>
+		/// <param name="read"></param>
+		/// <returns></returns>
 		public static async Task<List<U>> ReadParallel<T, U>(List<T> inputs, Func<T, Task<U>> read)
 		{
 			var readTasks = inputs.Select(read)
