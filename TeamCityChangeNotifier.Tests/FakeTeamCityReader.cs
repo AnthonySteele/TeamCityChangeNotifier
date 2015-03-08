f09084b4f6ff5017494ff22a0997cfb9423350ae
AnthonySteele <Anthony@PlasticAvalon.org>
2015-03-08 21:03:02 +0000

    Added skelteon to test on TeamCityOperations

---
+++ TeamCityChangeNotifier.Tests/FakeTeamCityReader.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+
+using TeamCityChangeNotifier.Http;
+
+namespace TeamCityChangeNotifier.Tests
+{
+	public class FakeTeamCityReader : ITeamCityReader
+	{
+		private string _value;
+
+		public FakeTeamCityReader(string value)
+		{
+			_value = value;
+		}
+
+		public Task<string> ReadBuildList(string buildName)
+		{
+			return Task.FromResult(_value);
+		}
+
+		public Task<string> ReadBuild(int buildId)
+		{
+			return Task.FromResult(_value);
+		}
+
+		public Task<string> ReadBuildChanges(int buildId)
+		{
+			return Task.FromResult(_value);
+		}
+
+		public Task<string> ReadChange(int changeId)
+		{
+			return Task.FromResult(_value);
+		}
+
+		public Task<string> ReadRelativeUrl(string relativeUrl)
+		{
+			return Task.FromResult(_value);
+		}
+	}
+}
---
+++ TeamCityChangeNotifier.Tests/TeamCityOperationsTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+using TeamCityChangeNotifier.Args;
+
+namespace TeamCityChangeNotifier.Tests
+{
+	[TestFixture]
+	public class TeamCityOperationsTests
+	{
+		[Test]
+		public void CanCreate()
+		{
+			var request = new Request
+				{
+					InitialBuildId = 1234,
+					TeamCityUser = "fred",
+					TeamCityPassword = "hunter2"
+				};
+
+			var operations = new TeamCityOperations(new FakeTeamCityReader("foo"), request);
+
+			Assert.That(operations, Is.Not.Null);
+		}
+	}
+}
